f35aacaaba3bd1a85854076d32c12b9ef27401b7
Ariel Blasi <exorcismo@gmail.com>
2017-10-04 19:28:52 -0300

    Add more fun fun functions

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const cost = document.querySelector('#cost')
 const tipPercent = document.querySelector('#tipPercent')
 const tip = document.querySelector('#tip')
+const peoples = document.querySelector('#peoples')
+const people = document.querySelector('#people')
+const total = document.querySelector('#total')
 
 cost.addEventListener('keyup', tipcalc)
-tipPercent.addEventListener('keyup', tipcalc)
+tipPercent.addEventListener('keyup', tipCalc)
+peoples.addEventListener('keyup', peopleCalc)
 
-function tipcalc() {
+function tipCalc() {
 	tip.value = cost.value * (tipPercent.value / 100 || 0.1)
 }
+
+function peopleCalc() {}
+
+function totalCalc() {}
